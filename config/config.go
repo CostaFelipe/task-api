32fdaae8378a4f60fbc0ad97e3bd0ea7c03d9d58
wedo <costadefelipe@gmail.com>
2026-01-16 14:39:33 -0300

    add config variaveis

---
+++ config/config.go
@@ -0,0 +1,13 @@
+package config
+
+type Config struct {
+	DBDriver      string
+	DBHost        string
+	DBPort        string
+	DBUser        string
+	DBPassword    string
+	DBName        string
+	WebServerPort string
+	JWTSecret     string
+	JWTExpiresIn  int
+}
