415482a9feee74cf793788d9e9f510a3b4bf5189
wedo <costadefelipe@gmail.com>
2026-01-20 09:00:14 -0300

    repository user: create, findbyid

--- internal/repository/user_repository.go
+++ internal/repository/user_repository.go
@@ -21,6 +21,33 @@ func NewUserRepositoy(db *sql.DB) *UserRepository {
 	}
 }
 
+func (u *UserRepository) Create(ctx context.Context, user *entity.User) error {
+	var exists bool
+	err := u.DB.QueryRowContext(ctx, "SELECT EXISTS(SELECT 1 FROM users WHERE email=?)", user.Email).Scan(&exists)
+	if err != nil {
+		return err
+	}
+
+	if exists {
+		return ErrEmailExists
+	}
+
+	query := `INSERT INTO users (name, email, password) VALUES (?,?,?)`
+
+	result, err := u.DB.ExecContext(ctx, query, user.Name, user.Email, user.Password)
+	if err != nil {
+		return err
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return err
+	}
+
+	user.ID = int(id)
+	return nil
+}
+
 func (u *UserRepository) FindByEmail(ctx context.Context, email string) (*entity.User, error) {
 	query := `SELECT id, name, email, password, created_at, updated_at FROM users WHERE email = ?`
 
@@ -40,6 +67,22 @@ func (u *UserRepository) FindByEmail(ctx context.Context, email string) (*entity
 		}
 		return nil, err
 	}
+
+	return user, nil
+}
+
+func (u *UserRepository) FindByID(ctx context.Context, id int) (*entity.User, error) {
+	query := `SELECT id, name, email, password, created_at, updated_at FROM users WHERE id = ?`
+
+	user := &entity.User{}
+
+	err := u.DB.QueryRowContext(ctx, query, id).Scan(
+		&user.ID,
+		&user.Name,
+		&user.Email,
+		&user.Password,
+		&user.CreatedAt,
+		&user.UpdatedAt)
 
 	return user, nil
 }
