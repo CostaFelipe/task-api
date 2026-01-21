8fa384608952dd6660951ee78aa97c0df5cba347
wedo <costadefelipe@gmail.com>
2026-01-21 11:12:52 -0300

    create task repository FindByID

--- internal/repository/task_repository.go
+++ internal/repository/task_repository.go
@@ -3,9 +3,14 @@ package repository
 import (
 	"context"
 	"database/sql"
+	"errors"
 	"time"
 
 	"github.com/CostaFelipe/task-api/internal/entity"
+)
+
+var (
+	errTaskNotFound = errors.New("Task not found!")
 )
 
 type TaskRepository struct {
@@ -35,4 +40,35 @@ func (t *TaskRepository) Create(ctx context.Context, task *entity.Task) error {
 	task.CreatedAt = time.Now()
 	task.UpdatedAt = time.Now()
 	return nil
+}
+
+func (t *TaskRepository) FindByID(ctx context.Context, id, userID int) (*entity.Task, error) {
+
+	query := `SELECT id, title, description, completed, priority, due_date, created_at, updated_at FROM users WHERE id=? AND user_id=?`
+
+	task := &entity.Task{}
+	var dueDate sql.NullTime
+
+	err := t.DB.QueryRowContext(ctx, query, id, userID).Scan(
+		&task.ID,
+		&task.Title,
+		&task.Description,
+		&task.Completed,
+		dueDate,
+		&task.Priority,
+		&task.CreatedAt,
+		&task.UpdatedAt,
+	)
+
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, errTaskNotFound
+		}
+	}
+
+	if dueDate.Valid {
+		task.DueDate = &dueDate.Time
+	}
+
+	return task, nil
 }
