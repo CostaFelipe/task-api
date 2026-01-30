9e96a7ae63d5d12913aa7f4ba983ef2e91aa323f
wedo <costadefelipe@gmail.com>
2026-01-30 13:45:24 -0300

    corrigindo erros e testando

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,15 +1,19 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 
 	"github.com/CostaFelipe/task-api/config"
 	"github.com/CostaFelipe/task-api/internal/database"
+	"github.com/CostaFelipe/task-api/internal/repository"
 )
 
 func main() {
 	cfg, err := config.LoadConfig()
+	ctx := context.Background()
+
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -20,4 +24,44 @@ func main() {
 	}
 
 	fmt.Println(db.Ping())
+
+	/*user := entity.User{
+		Name:     "Jonh Doe",
+		Email:    "jdoe@gmail.com",
+		Password: "1234567",
+	}*/
+
+	//dueDate := time.Now().Add(48 * time.Hour)
+
+	/*task := entity.Task{
+		Title:       "Aprender Go",
+		Description: "Estudar a linguagem Go.",
+		Priority:    "low",
+		DueDate:     &dueDate,
+		UserID:      1,
+	}*/
+
+	//userDb := repository.NewUserRepositoy(db)
+	taskDb := repository.NewTaskRepository(db)
+
+	/*err = userDb.Create(ctx, &user)
+	if err != nil {
+		fmt.Println("inserir dado ao banco deu errado")
+	}
+	*/
+
+	/*err = taskDb.Create(ctx, &task)
+	if err != nil {
+		fmt.Println("erro ao inserir task no banco deu errado")
+	}*/
+
+	taskID := 1
+	userID := 1
+
+	task, err := taskDb.FindByID(ctx, taskID, userID)
+	if err != nil {
+		fmt.Println("error ao buscar task")
+	}
+
+	fmt.Print(task)
 }
