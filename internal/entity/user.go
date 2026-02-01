b4ffc092763b6112e205dd3c0cb712586af257f5
wedo <costadefelipe@gmail.com>
2026-02-01 15:19:20 -0300

    criando NewUser(refatorando)

--- internal/entity/user.go
+++ internal/entity/user.go
@@ -12,3 +12,13 @@ type User struct {
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
+
+func NewUser(name, email, password string) *User {
+	return &User{
+		Name:      name,
+		Email:     email,
+		Password:  password,
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	}
+}
