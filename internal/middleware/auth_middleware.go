691aa5dd2f279ed3244f06dd1cf37cab6610336d
wedo <costadefelipe@gmail.com>
2026-02-05 09:03:37 -0300

    create GenerateToken

--- internal/middleware/auth_middleware.go
+++ internal/middleware/auth_middleware.go
@@ -1,6 +1,8 @@
 package middleware
 
 import (
+	"time"
+
 	"github.com/CostaFelipe/task-api/config"
 	"github.com/golang-jwt/jwt/v5"
 )
@@ -23,4 +25,18 @@ func NewAuthMiddleware(cfg config.Config) *AuthMiddleware {
 	return &AuthMiddleware{
 		jwtConfig: cfg,
 	}
+}
+
+func (h *AuthMiddleware) GenerateToken(userId int, email string) (string, error) {
+	claims := &UserClaims{
+		UserID: userId,
+		Email:  email,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Duration(h.jwtConfig.JWTExpiresIn) * time.Minute)),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte(h.jwtConfig.JWTSecret))
 }
