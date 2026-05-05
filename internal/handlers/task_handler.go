a24647e1dbe4ecb90f42c3daa583414e93963b25
wedo <costadefelipe@gmail.com>
2026-05-05 16:57:47 -0300

    refatorando errors

--- internal/handlers/task_handler.go
+++ internal/handlers/task_handler.go
@@ -70,7 +70,7 @@ func (h *TaskHandler) GetByID(w http.ResponseWriter, r *http.Request) {
 
 	task, err := h.taskRepo.FindByID(r.Context(), id, userId)
 	if err != nil {
-		if errors.Is(err, repository.ErrUserNotFound) {
+		if errors.Is(err, repository.ErrTaskNotFound) {
 			util.ResponseJSON(w, http.StatusInternalServerError, responses.ErrorResponse{Error: "task não encontrada"})
 			return
 		}
